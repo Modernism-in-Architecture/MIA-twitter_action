5c674fe4ec437e0f434777cc4cc39e34bc49e053
Norma Driske <norma@entwicklerheld.de>
2022-08-20 23:11:04 +0200

    Add main function

--- main.py
+++ main.py
@@ -1,8 +1,68 @@
 from api_clients.twitter_client import TwitterAPI
 from api_clients.mia_client import MiaAPI
+import logging
+import logging.handlers
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+
+def main() -> bool:
+
+    mia_response, was_successful = MiaAPI.get_mia_building_details()
+
+    if not was_successful:
+        logger.error(
+            f"Retrieving building details failed. API error occured. ERROR: {mia_response}"
+        )
+        return False
+
+    if mia_response.status_code != 200:
+        logger.error(
+            f"Retrieving building details was not successful. STATUS: {mia_response.status_code}",
+        )
+        return False
+
+    building_details = mia_response.json().get("data")
+
+    building_id = building_details.get("id")
+    title = building_details.get("name")
+    year = building_details.get("yearOfConstruction")
+    city = building_details.get("city")
+    country = building_details.get("country")
+    architect = building_details.get("architect")
+    url = building_details.get("absoluteURL")
+
+    tweet_content = f"""New on MIA: {title} by {architect} ({year}) in {city}, {country}\n#modernism #architecture #bauhaus #neuesbauen #internationalstyle #interwararchitecture #{city} #{country}\n{url}"""
+
+    twitter_response, was_successful = TwitterAPI.tweet_mia_building(tweet_content)
+
+    if not was_successful:
+        logger.error(f"Post tweet failed. API error occured. ERROR: {twitter_response}")
+        return False
+
+    mia_response, was_successful = MiaAPI.set_building_published_on_twitter(building_id)
+
+    if not was_successful:
+        logger.error(
+            f"Set building published failed. API error occured. ERROR: {mia_response}"
+        )
+        return False
+
+    logger.info(f"Successfully published {title} on twitter.")
+
+    return True
 
 
 if __name__ == "__main__":
-    pass
-
-
+    main()
